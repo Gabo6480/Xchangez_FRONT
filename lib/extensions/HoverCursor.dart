1d22862be0913954f259c77974fc0308839ea737
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-09-17 16:58:36 -0500

    Se añadió HoverCursor
    HoverCursor es un wrapper que sirve para que sus hijos causen el cambio del cursor a pointer

---
+++ lib/extensions/HoverCursor.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'dart:html' as html;
+
+class HoverCursor extends MouseRegion {
+  static final appContainer =
+      html.window.document.querySelectorAll('flt-glass-pane')[0];
+  HoverCursor({Widget child})
+      : super(
+            onHover: (PointerHoverEvent evt) {
+              appContainer.style.cursor = 'pointer';
+            },
+            onExit: (PointerExitEvent evt) {
+              appContainer.style.cursor = 'default';
+            },
+            child: child);
+}
