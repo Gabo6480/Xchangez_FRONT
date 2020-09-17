0e9a4ec7e12641bfa680f835a251530157ce27c2
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-09-17 14:45:53 -0500

    Versión mejorada

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -1,6 +1,7 @@
 import 'dart:html';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -26,67 +27,108 @@ class _LoginPageState extends State<LoginPage> {
 
     return Scaffold(
         backgroundColor: theme.backgroundColor,
-        body: Center(
-            child: Column(
-          children: [
-            Image.asset(
-              'assets/images/Xchangez_no_background_OW.png',
-              scale: 1.30,
-            ),
-            Container(
-                width: width,
-                padding: EdgeInsets.all(25),
-                decoration: BoxDecoration(
-                    shape: BoxShape.rectangle,
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(5)),
-                child: Form(
-                    key: formKey,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 8),
-                            suffixIcon: Icon(Icons.person),
-                            labelText: "Correo",
-                          ),
-                          onSaved: (input) => _email = input,
-                          validator: (input) {
-                            RegExp regex = new RegExp(
-                                r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
-                            if (!regex.hasMatch(input))
-                              return 'Ingrese un Correo Válido';
-                            else if (loginFail) {
-                              loginFail = false;
-                              return "Revise su correo o la contraseña";
-                            }
-                            return null;
-                          },
-                          keyboardType: TextInputType.emailAddress,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
+        body: Stack(children: [
+          Container(
+            color: theme.primaryColor,
+            width: double.infinity,
+            height: mQuery.size.height * .27,
+          ),
+          Center(
+              child: Column(
+            children: [
+              SizedBox(
+                height: mQuery.size.height * .02,
+              ),
+              Image.asset(
+                'assets/images/Xchangez_no_background_OW.png',
+                scale: 1.30,
+              ),
+              Container(
+                  width: width,
+                  padding: EdgeInsets.all(25),
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(5)),
+                  child: Form(
+                      key: formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            decoration: const InputDecoration(
                               contentPadding: EdgeInsets.only(left: 8),
-                              suffixIcon: Icon(Icons.lock),
-                              labelText: "Contraseña"),
-                          obscureText: true,
-                        ),
-                        SizedBox(
-                          height: 10,
-                        ),
-                        SizedBox(
-                            width: double.infinity,
-                            child: RaisedButton(
-                                padding: EdgeInsets.symmetric(vertical: 15),
-                                child: Text("Ingresar"),
-                                onPressed: () {}))
-                      ],
-                    )))
-          ],
-        )));
+                              suffixIcon: Icon(Icons.person),
+                              labelText: "Correo",
+                            ),
+                            onSaved: (input) => _email = input,
+                            validator: (input) {
+                              RegExp regex = new RegExp(
+                                  r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
+                              if (!regex.hasMatch(input))
+                                return 'Ingrese un Correo Válido';
+                              else if (loginFail) {
+                                loginFail = false;
+                                return "Revise su correo o la contraseña";
+                              }
+                              return null;
+                            },
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          TextFormField(
+                            decoration: const InputDecoration(
+                                contentPadding: EdgeInsets.only(left: 8),
+                                suffixIcon: Icon(Icons.lock),
+                                labelText: "Contraseña"),
+                            obscureText: true,
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          SizedBox(
+                              width: double.infinity,
+                              child: RaisedButton(
+                                  padding: EdgeInsets.symmetric(vertical: 15),
+                                  child: Text("Ingresar"),
+                                  onPressed: () {})),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          RichText(
+                              text: TextSpan(
+                            text: "¿Olvidaste tu contraseña?",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 13,
+                                decoration: TextDecoration.underline),
+                            recognizer: new TapGestureRecognizer()
+                              ..onTap = () {},
+                          )),
+                          Divider(
+                            color: Colors.black26,
+                            thickness: 1,
+                          ),
+                          RichText(
+                              text: TextSpan(
+                                  text: "¿No tienes una cuenta? ",
+                                  style: TextStyle(color: Colors.grey),
+                                  children: [
+                                TextSpan(
+                                  text: "Regístrate",
+                                  style: TextStyle(
+                                      color: theme.accentColor,
+                                      decoration: TextDecoration.underline),
+                                  recognizer: new TapGestureRecognizer()
+                                    ..onTap = () {},
+                                )
+                              ]))
+                        ],
+                      )))
+            ],
+          ))
+        ]));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,8 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.green,
+        primaryColor: Color(0xff03435c),
+
         buttonTheme: ButtonThemeData(
             buttonColor: Color(0xffff7200), textTheme: ButtonTextTheme.primary),
 
