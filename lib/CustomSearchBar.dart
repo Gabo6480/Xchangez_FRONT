c2e6419f6f42c688cd0e4d4b2e4320a520e26a8c
Gabo6480 <60194781+Gabo6480@users.noreply.github.com>
2020-09-22 14:24:54 -0500

    Inicio de la SearchBar

---
+++ lib/CustomSearchBar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomSearchBar extends StatefulWidget {
+  CustomSearchBar({Key key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _CustomSearchBarState();
+}
+
+class _CustomSearchBarState extends State<CustomSearchBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
